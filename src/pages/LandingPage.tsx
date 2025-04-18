c6b3a0e37c97954f2fe2843f509f78214dd5447b
ykl1 <lyoungkwang.yl@gmail.com>
2025-04-18 16:20:50 -0400

    Add google form link

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -92,6 +92,22 @@ const LandingPage = () => {
               </svg>
               <span>linkedIn</span>
             </a>
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLScZ0cSxMSAYNhJxcw757hBjZqvR38eNb3pSI1XQkwHX3DWwmQ/viewform?usp=header"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center text-white hover:text-white/90 transition"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-5 h-5 mr-2"
+              >
+                <path d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"></path>
+              </svg>
+              <span>give feedback</span>
+            </a>
           </div>
           <div className="mt-2">
             <span className="text-md">Built with ♥</span>
